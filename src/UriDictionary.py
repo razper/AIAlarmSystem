b1e9966b3115e0d5c8526ce54bd105945e48f429
Or Eli Pilosof <44499215+orpilosof21@users.noreply.github.com>
2019-12-08 18:32:20 +0200

    basic face detec

--- src/UriDictionary.py
+++ src/UriDictionary.py
@@ -1,4 +1,4 @@
 import sys
-token = sys.argv[1]
+token = sys.argv[0]
 PRIMARY_CAMERA = 0
 EXTERNAL_CAMERA = 1
--- src/opencvtest.py
+++ src/opencvtest.py
@@ -6,8 +6,11 @@ if __name__ == '__main__':
     cap = cv2.VideoCapture(EXTERNAL_CAMERA)
     cascPath = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'haarcascade_frontalface_default.xml')
     faceCascade = cv2.CascadeClassifier(cascPath)
-
+    rects_value_by_frame=0
+    faces_iter = 0
     while True:
+        rects=[]
+
         # Capture frame-by-frame
         ret, frame = cap.read()
 
@@ -23,10 +26,23 @@ if __name__ == '__main__':
 
         # Draw a rectangle around the faces
         for (x, y, w, h) in faces:
+            rect=(x,y,w,h)
+            rects.append(rect)
             cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
         # Display the resulting frame
         cv2.imshow('Video', frame)
 
+        if (len(rects)>rects_value_by_frame):
+            print("new face "+str(faces_iter))
+            """
+            In here we should print out/send to bot the diff from the end.
+            e.g. if rects_value_by_frame = 2 
+            len(rects) =5
+            then we should send a pic of the last 3, index 2,3,4
+            those are the new faces there were added
+            """
+            faces_iter+=1
+        rects_value_by_frame=len(rects)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
